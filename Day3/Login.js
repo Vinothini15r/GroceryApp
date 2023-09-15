b5242ea45b0e9a0b21f3a4073ac43b5570145eb1
Vinothini15r <120247771+Vinothini15r@users.noreply.github.com>
2023-09-15 15:13:19 +0530

    Login.js

---
+++ Day3/Login.js
@@ -0,0 +1,48 @@
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import "./Login.css";
+
+export default function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleLogin = (e) => {
+    e.preventDefault();
+    console.log("Email:", email);
+    console.log("Password:", password);
+  };
+
+  return (
+    <div className="body">
+    <div className="login-container">
+      <h2>Login</h2>
+      <form onSubmit={handleLogin}>
+        <div className="form-group">
+          <label>Email</label>
+          <input 
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            placeholder="Enter your email"
+            required
+          />
+        </div>
+        <div className="form-group">
+          <label>Password</label>
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            placeholder="Enter your password"
+            required
+          />
+        </div>
+        <button type="submit">Login</button>
+      </form>
+      <p>
+        Don't have an account? <Link className="link" to="/signup">Sign Up</Link>
+      </p>
+    </div>
+    </div>
+  );
+}
